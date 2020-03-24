11e0854dd1db93374acfb23a00d9acee0b28ef36
Demons Light <jetaimefrc@gmail.com>
2020-03-24 12:10:52 +0700

    feat: add error handler middleware

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,10 @@
+import { generalErrors } from '@helpers/errorHandlers'
+import { INTERNAL_SERVER_ERROR } from 'http-status-codes'
+
+const errorHandlerMiddleware = (app) => {
+  app.use((err, req, res, next) => {
+    return generalErrors(res, INTERNAL_SERVER_ERROR, err.message)
+  })
+}
+
+export default errorHandlerMiddleware
