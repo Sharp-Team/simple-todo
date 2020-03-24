00df6e438f09e3854461c6b405a7c1eb429f0da2
Demons Light <jetaimefrc@gmail.com>
2020-03-24 12:02:28 +0700

    feat: add basic todo controller

---
+++ src/controllers/Todo.js
@@ -0,0 +1,42 @@
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const getAllTodosCurrentUser = (req, res, next) => {}
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const getTodoById = (req, res, next) => {}
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const postTodo = (req, res, next) => {}
+
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const putTodoById = (req, res, next) => {}
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const deleteTodoById = (req, res, next) => {}
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const deleteAllTodos = (req, res, next) => {}
